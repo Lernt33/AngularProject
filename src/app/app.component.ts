535148c33ee60042758b95a362abd07afa2a823a
Lernt <giorgizautashvili24@gmail.com>
2024-05-27 17:00:50 +0400

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '@app-shared/ui/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, NavComponent],
 })
 export class AppComponent {
   title = 'FinalProject';
