90d5c04d1db0281209e8666e5f866cb39f41dfe7
Lernt <giorgizautashvili24@gmail.com>
2024-06-01 12:48:10 +0400

    home page almost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from '@app-shared/ui/nav/nav.component';
 import { HomeComponent } from './features/home/home.component';
+import { FooterComponent } from './shared/ui/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'FinalProject';
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -12,12 +12,8 @@ import { MovieComponent } from '@app-shared/ui/movie/movie.component';
 export class HomeComponent implements OnInit {
   service = inject(HttpService);
   public list!: Array<movie>;
-  public clicks: number = 0;
   ngOnInit(): void {
     this.service.getFilms().subscribe((data: any) => (this.list = data));
     console.log(this.list);
-  }
-  clickedOnInput() {
-    this.clicks += 1;
   }
 }
