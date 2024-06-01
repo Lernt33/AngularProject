65988bea36d0f2664568e4c2350aac868a672170
Lernt33 <giorgizautashvili24@gmail.com>
2024-06-01 22:25:23 +0400

    Full navbar

--- src/app/shared/ui/nav/nav.component.ts
+++ src/app/shared/ui/nav/nav.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-
+  title= 'mass|records'
+  public sidebaropened:boolean=false
+  header = 'assets/imgs/fc7570_24323e7a91004da9b2a3b6472b07c036~mv2.png'
+  switch(){
+    this.header='assets/imgs/fc7570_4af2d8a34d6f4916991f7f6b85fc2922~mv2.png'
+  }
+  switchback(){
+    this.header='assets/imgs/fc7570_24323e7a91004da9b2a3b6472b07c036~mv2.png'
+  }
+  sidebar(){
+    this.sidebaropened=!this.sidebaropened
+  }
 }
