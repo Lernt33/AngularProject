e5cd9b7c03c48e3533c5ea85a5adf0c244d3f0ca
Lernt33 <giorgizautashvili24@gmail.com>
2024-06-02 00:51:18 +0400

    responsive made

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { HttpService } from '@app-shared/services/http.service';
 import { movie } from '@app-shared/interfaces';
 import { MovieComponent } from '@app-shared/ui/movie/movie.component';
+import { FormsModule,} from '@angular/forms'
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -12,6 +13,7 @@ import { MovieComponent } from '@app-shared/ui/movie/movie.component';
 export class HomeComponent implements OnInit {
   service = inject(HttpService);
   public list!: Array<movie>;
+  formData = {email:''}
   ngOnInit(): void {
     this.service.getFilms().subscribe((data: any) => (this.list = data));
     console.log(this.list);
